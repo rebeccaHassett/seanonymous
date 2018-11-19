beff5f1c7bcb0c53c0a1431bc9e99d7ff09ac29c
Andrew Jaffie <andrew.jaffie@stonybrook.edu>
2018-11-19 07:08:25 +0000

    Messing around with Flask at 2 am

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
-from flask import Flask
-from flask_socketio import SocketIO
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send   #, #join_
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+ROOMS = {} #track active rooms
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+def messageReceived(methods=['Get', 'Post']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['Get', 'Post']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
+    
