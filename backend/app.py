c554d025aef077864908486cebfe8cd4f7fdb922
Seanonymous VM <seanonymous@andrewjaffie.me>
2018-11-24 23:10:40 +0000

    Store Form JSON Function

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send   #, #join_
 import json
 import os
+from database import store_form_data, initDB
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ["FLASK_SECRET"] or 'secret!'
+app.config['SECRET_KEY'] #= os.environ["FLASK_SECRET"] or 'secret!'
 socketio = SocketIO(app)
 connected_clients = []
 
@@ -24,4 +25,10 @@ def handle_my_custom_event(json, methods=['Get', 'Post']):
 
 
 if __name__ == "__main__":
+    initDB()
+    with open("../sample_ext_to_srv.json") as f:
+        clientid = 123
+        data = json.load(f)
+        store_form_data(data["forms"][0], clientid)
     socketio.run(app, debug=True)
+
--- backend/database.py
+++ backend/database.py
@@ -4,12 +4,13 @@ import json
 conn = None
 
 def initDB():
-    if(conn == None):
-        conn = pymysql.connect(host='localhost', port= 3306, user='root', passwd='seanonymous', db='cse331')
+    global conn
+    conn = conn or pymysql.connect(host='localhost', port= 3306, user='root', passwd='seanonymous', db='cse331')
+
 def getCursor():
-    if(conn == None):
-        initDB()
-    return conn.getCursor()
+    global conn
+    initDB()
+    return conn.cursor()
 
 """
 param data: form from extension->server payload in the form of a dictionary. key-value pairs are
@@ -19,38 +20,46 @@ Then, for each remotedef: value pair looks for a mapping from query results. if 
     in the dict. if it is found, the data is stored in the appropriate place. (Credentials, CreditCard, etc.)
 Remaining values are stored in the ComplexForms table after being converted back into a json string.
 """
-def store_form_data(data):
+def store_form_data(data, clientid):
     url = data.pop("url", None)
     if url == None:
         return -1
-    reducedURL = url.split('/')[0] + "//" + url.split('/')[2]
-    print(reducedURL)
-   """ cur = getCursor()
-    phoneNumber = data["forms"]["phone"]
-    address = data["forms"]["Address"]
-    firstName = data["forms"]["FirstName"]
-    lastName = data["forms"]["LastName"]
-    birthDate = data["forms"]["BirthDate"]
-    email = data["forms"]["Email"]
-    ssn = data["forms"]["SSN"]
-    id = data["clientid"]
-    cur.execute('SELECT ID FROM Client C WHERE ID = (%s)', id)
-    if(cur.rowcount == 0):
-        cur.execute('INSERT INTO Client(Id, CellPhone, Address, Email, SSN, FirstName, LastName, BirthDate, NextPayload) VALUES(%s, %s, %s,%s, %s, %s, %s, %s, "")', (id, phoneNumber, address, email, ssn, firstName, lastName, birthDate)) 
-        conn.commit()   
-    else:
-        if(phoneNumber != ""):
-            cur.execute('UPDATE Client SET CellPhone = (%s) WHERE Id = (%s)', (id, phoneNumber))
-        if(address != ""):
-            cur.execute('UPDATE Client SET Address = (%s) WHERE Id = (%s)', (id, address))
-        if(email != ""):
-            cur.execute('UPDATE Client SET Email = (%s) WHERE Id = (%s)',(id, email))
-        if(ssn != ""):
-            cur.execute('UPDATE Client SET SSN = (%s)  WHERE Id = (%s)',(id, ssn))
-        if(firstName != ""):
-            cur.execute('UPDATE Client SET FirstName = (%s) WHERE Id = (%s)',(id, firstName))
-        if(lastName != ""):
-            cur.execute('UPDATE Client SET LastName = (%s) WHERE Id = (%s)',(id, lastName))
-        if(birthDate != ""):
-            cur.execute('UPDATE Client SET BirthDate = (%s) WHERE Id = (%s)',(id, birthDate))
-        conn.commit()"""
+    reducedURL = url.split('/')[0] + "//" + url.split('/')[2] + "/"
+    cur = getCursor()
+    cur.execute('SELECT * FROM FormIDMappings WHERE URL = (%s) OR URL = (%s)', (reducedURL, "*"))
+    test = cur.fetchall()
+    #populate dictionary and pass dictionart to separate function to store data in database
+    for row in test:
+        val = data.pop(test[0][2], None)
+        print(val)
+        cur.execute('UPDATE Client SET (%s) = (%s) WHERE Id = (%s)', (CellPhone, val, clientid))
+        cur.commit()
+""" cur = getCursor()
+phoneNumber = data["forms"]["phone"]
+address = data["forms"]["Address"]
+firstName = data["forms"]["FirstName"]
+lastName = data["forms"]["LastName"]
+birthDate = data["forms"]["BirthDate"]
+email = data["forms"]["Email"]
+ssn = data["forms"]["SSN"]
+id = data["clientid"]
+cur.execute('SELECT ID FROM Client C WHERE ID = (%s)', id)
+if(cur.rowcount == 0):
+cur.execute('INSERT INTO Client(Id, CellPhone, Address, Email, SSN, FirstName, LastName, BirthDate, NextPayload) VALUES(%s, %s, %s,%s, %s, %s, %s, %s, "")', (id, phoneNumber, address, email, ssn, firstName, lastName, birthDate)) 
+conn.commit()   
+else:
+if(phoneNumber != ""):
+cur.execute('UPDATE Client SET CellPhone = (%s) WHERE Id = (%s)', (id, phoneNumber))
+if(address != ""):
+cur.execute('UPDATE Client SET Address = (%s) WHERE Id = (%s)', (id, address))
+if(email != ""):
+cur.execute('UPDATE Client SET Email = (%s) WHERE Id = (%s)',(id, email))
+if(ssn != ""):
+cur.execute('UPDATE Client SET SSN = (%s)  WHERE Id = (%s)',(id, ssn))
+if(firstName != ""):
+cur.execute('UPDATE Client SET FirstName = (%s) WHERE Id = (%s)',(id, firstName))
+if(lastName != ""):
+cur.execute('UPDATE Client SET LastName = (%s) WHERE Id = (%s)',(id, lastName))
+if(birthDate != ""):
+cur.execute('UPDATE Client SET BirthDate = (%s) WHERE Id = (%s)',(id, birthDate))
+conn.commit()"""
